0f823610d3e08984746cbba608b8e98c9c1b095d
Kelino <125823968+Kelino-Kelvin@users.noreply.github.com>
2025-11-29 18:39:12 +0100

    initial commit: todo list logic

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { FullList } from "./TodoList"
+
+function App() {
+
+  return (
+    <>
+      <header className='bg-purple-700 text-white p-2'>
+        <h1 className='text-xl'>
+          Todo List Plus
+        </h1>
+      </header>
+
+      <FullList />
+    </>
+  )
+}
+
+export default App
